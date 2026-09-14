428ea47cd0c78572b5ef578503128734c7086816
Nelvine-Kirwa <112783599+Nelvine-Kirwa@users.noreply.github.com>
2026-09-14 19:40:55 +0300

    Working build: app runs on Infinix X6528, com.ev.radar, geocoding fix, JVM ignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,23 +1,23 @@
-﻿plugins {
+plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.ev_radar"
+    namespace = "com.ev.radar"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
 
     compileOptions {
-isCoreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.ev_radar"
+        applicationId = "com.ev.radar"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
